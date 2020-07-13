88664ad0650a19a8d1e3535df84dd731a504b003
david <davidabu91@gmail.com>
2020-07-13 13:46:36 +0300

    Updates

---
+++ precache-manifest.3514fbf4b72e5f0243e0accb0d2ad90b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ce9f4dab5087f3fb0819ba8a9d7b234",
+    "url": "/Security-management/index.html"
+  },
+  {
+    "revision": "49d8ea0893025f2023b0",
+    "url": "/Security-management/static/css/2.1417c86a.chunk.css"
+  },
+  {
+    "revision": "a77d949edc06bdd32ca4",
+    "url": "/Security-management/static/css/main.85d78891.chunk.css"
+  },
+  {
+    "revision": "49d8ea0893025f2023b0",
+    "url": "/Security-management/static/js/2.be4b318e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Security-management/static/js/2.be4b318e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a77d949edc06bdd32ca4",
+    "url": "/Security-management/static/js/main.15660a91.chunk.js"
+  },
+  {
+    "revision": "820fda8761145d37ec78",
+    "url": "/Security-management/static/js/runtime-main.0677e360.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Security-management/static/media/logo.5d5d9eef.svg"
+  }
+]);
